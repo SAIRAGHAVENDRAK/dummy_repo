804e2e61283d9c05ea18e60d9f5130e3e5e364b6
SAIRAGHAVENDRAK <138328528+SAIRAGHAVENDRAK@users.noreply.github.com>
2023-07-04 00:59:12 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+import os
+
+key = "sbdkakkakdbasdbas"
+password = "dsbkjdafhdskfksd"
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        contents = file.read()
+        print(contents)
+
+def main():
+    filename = 'test.txt'
+    if os.path.exists(filename):
+        read_file(filename)
+    else:
+        print(f"File '{filename}' does not exist.")
+
+if __name__ == '__main__':
+    main()
